a95764d77464a2c85e72cd34495b4d4f0ec10a1b
dasiklasik <daryasamsonowicz@gmail.com>
2020-10-23 14:27:44 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let array = str.split('');
+
+    let part = [];
+    let partStr = [];
+    let res = false;
+    let partRes = 0;
+    let i = 0;
+
+    while (i < bracketsConfig.length) {
+        part = bracketsConfig[i].join('');
+        i++;
+        let j = 0;
+
+        while (j < str.length) {
+            partRes = str.indexOf(part);
+            j++;
+            if (partRes === -1) {
+                res = false;
+                j++;
+                break;
+            } else {
+                partStr = array.splice(partRes, 2);
+                str = array.join('');
+                res = true;
+                j = 0;
+            }
+        }
+
+
+    }
+    return res;
 }
